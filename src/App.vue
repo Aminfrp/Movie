aaa57161d69d16afc895d07cd1cdb1a8082b1949
amin firouzpour <a.firouzpour@novin.dev>
2022-08-25 08:15:27 +0430

    feat: :tada: create the base application

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <LayoutsComponent />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import LayoutsComponent from "@/components/layouts/LayoutsComponent.vue";
+
+export default defineComponent({
+  name: "AppView",
+  components: {
+    LayoutsComponent,
+  },
+});
+</script>
---
+++ src/components/layouts/LayoutsComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <HeaderComponent />
+  <router-link to="/">Home</router-link>
+  <router-link to="/about">About</router-link>
+  <router-view />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import HeaderComponent from "./HeaderComponent.vue";
+
+export default defineComponent({
+  name: "LayoutsComponent",
+  components: { HeaderComponent },
+});
+</script>
